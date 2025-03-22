15a41b1a7b1ae4a54da56223e490a0ca686fdff1
seahpark247 <famousjedo@gmail.com>
2025-03-21 20:52:21 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Seah Park on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
