116741c64f80ff9b50c2cca96f729a9f9d7bacfe
seahpark247 <famousjedo@gmail.com>
2025-03-23 14:12:05 -0500

    commit codes

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -5,26 +5,58 @@
 //  Created by Seah Park on 3/21/25.
 //
 
+import Observation
 import SwiftUI
 
-@Observable
-class User {
-    var firstName = "bilbo"
-    var lastName = "baggins"
-}
+//struct SecondView: View {
+//    @Environment(\.dismiss) var dismiss
+//    let name: String
+//    
+//    var body: some View {
+//        Text("Hello, \(name)")
+//        Button("Dismiss") {
+//            dismiss()
+//        }
+//    }
+//}
 
 struct ContentView: View {
-    @State private var user = User()
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+//    @State private var showingSheet = false
     
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-            
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
         }
-        .padding()
+        
+//        Button("Show Sheet") {
+//            showingSheet.toggle()
+//        }
+//        .sheet(isPresented: $showingSheet) {
+//            SecondView(name: "Seah")
+//        }
     }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+    }
+    
 }
 
 #Preview {
